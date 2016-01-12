557ca0f77303ce90a6264af103835b318dec00c9
Ravi Kumar Nimmi <rvkmr102@gmail.com>
2016-01-12 20:37:39 +0530

    jade file added

---
+++ src/public/javascripts/profile.js
@@ -0,0 +1,63 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var NotificationImage = React.createClass({
+    render: function(){
+    return(
+            <div>
+                <img src={image}></img>
+            </div>
+            );
+    }
+});
+
+var FileSelector = React.createClass({
+    render: function(){
+    return(
+            <div>
+                <div>
+                    <label>Upload Image</label>
+                </div>
+                <div>
+                    <label><font color="red">{err_msg}</font></label>
+                </div>
+                <form id="uploadForm"
+                      encType="multipart/form-data"
+                      action="/upload_image"
+                      method="post">
+                  <input type="file" name="userPhoto"></input>
+                  <input type="submit" value="Upload Image" name="submit"></input>
+                  <span id = "status"></span>
+                </form>
+            </div>
+            )
+    }
+});
+
+var Details = React.createClass({
+    render: function(){
+    return (
+            <div>
+                <h2> Profile </h2>
+                <h5><strong>id</strong>: {user.id}</h5>
+                <h5><strong>email</strong>: {user.email} </h5>
+                <h5><strong>website</strong>: {user.website} </h5>
+                <h4><a href="/send_permission_request"> send permission request </a></h4>
+                <h4><a href="/send_notification"> send notification </a></h4>
+                <h4><a href="/create_group"> create group</a></h4>
+                <h4><a href="/view_groups"> view groups</a></h4>
+                <h4><a href="/notification_analytics"> notification analytics</a></h4>
+                <h4><a href="/permission_analytics"> permission analytics</a></h4>
+                <h4><a href="/logout">logout</a></h4>
+            </div>
+            );
+    }
+});
+
+ReactDOM.render(
+       <div> 
+        <Details />
+        <NotificationImage />
+        <FileSelector />
+       </div>,
+document.getElementById('main'));
--- src/server/controllers/group.js
+++ src/server/controllers/group.js
@@ -4,16 +4,18 @@ import config from './../config/dashboard_config';
 
 var template_path = './../templates/dashboard/';
 var client_id ='568cf1896185571f9e3c071a';
+var test_user = {id: client_id, email: 'test email', website: 'test_website'};
+var test_image = 'image';
 
 function fetchAll(req, res, next){
     var params = {'client_id': client_id};
     request({url: config.get_groups_url, qs: params}, function (error, response, body) {
         if (!error && response.statusCode == 200) {
                 var groups = JSON.parse(body)['groups'];
-                res.render(template_path + 'view_groups.ejs', { website: req.user.website, groups: groups});
+                res.render(template_path + 'view_groups.jade', { website: req.user.website, groups: groups});
         }
         else{
-                res.render(template_path + 'profile.ejs');
+                res.render(template_path + 'profile.jade', {user: test_user, image: test_image, err_msg: ''});
         }
     });
 }
