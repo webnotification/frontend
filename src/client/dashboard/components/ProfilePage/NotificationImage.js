9abe5d4ae0c18afa66b59dbb77d40a7f1e5e9978
Ravi Kumar Nimmi <rvkmr102@gmail.com>
2016-01-27 18:14:17 +0530

    image display size changed

--- src/client/dashboard/components/ProfilePage/NotificationImage.js
+++ src/client/dashboard/components/ProfilePage/NotificationImage.js
@@ -3,7 +3,7 @@ class NotificationImage extends React.Component{
     render(){
         return(
             <div>
-                <img src={this.props.image}></img>
+                <img src={this.props.image} height="50" width="50"></img>
             </div>
         );
     }
