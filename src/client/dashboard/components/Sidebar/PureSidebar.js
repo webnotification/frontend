a92f188a919e0d570a718e4396957c571bd138fa
Ravi Kumar Nimmi <rvkmr102@gmail.com>
2016-02-03 23:47:08 +0530

    sidebar nested items added

--- src/client/dashboard/components/Sidebar/PureSidebar.js
+++ src/client/dashboard/components/Sidebar/PureSidebar.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import {LeftNav}   from 'material-ui';
 import { Link } from 'react-router';
-import {MenuItem, FlatButton}  from  'material-ui';
+import {ListItem, FlatButton}  from  'material-ui';
 
 
 class Sidebar extends React.Component{
   render(){
     return (
       <LeftNav docked={this.props.isDocked}>
-        <MenuItem linkButton={true} containerElement={<Link to="/dashboard/permission/send" />} primaryText="Send Permission" />
-        <MenuItem linkButton={true} containerElement={<Link to="/dashboard/notification/send" />} primaryText="Send Notification" />
-        <MenuItem linkButton={true} containerElement={<Link to="/dashboard/groups/create" />} primaryText="Create Groups" />
-        <MenuItem linkButton={true} containerElement={<Link to="/dashboard/groups/view" />} primaryText="View Groups" />
-        <MenuItem linkButton={true} containerElement={<Link to="/dashboard/analytics/notification" />} primaryText="Notification Analytics" />
-        <MenuItem linkButton={true} containerElement={<Link to="/dashboard/analytics/permission" />} primaryText="Permission Analytics" />
+        <ListItem primaryText="Permission Request" containerElement={<Link to="/dashboard/permission/send" />} />
+        <ListItem primaryText="Notification" containerElement={<Link to="/dashboard/notification/send" />} />
+        <ListItem
+            primaryText="Groups"
+            primaryTogglesNestedList={true}
+            nestedItems={[
+              <ListItem primaryText="Create" containerElement={<Link to="/dashboard/groups/create" />} />,
+              <ListItem primaryText="View"   containerElement={<Link to="/dashboard/groups/view" />} />,
+            ]}
+          />
+        <ListItem
+            primaryText="Analytics"
+            primaryTogglesNestedList={true}
+            nestedItems={[
+              <ListItem primaryText="Permission" containerElement={<Link to="/dashboard/analytics/permission" />} />,
+              <ListItem primaryText="Notification" containerElement={<Link to="/dashboard/analytics/notification" />} />,
+            ]}
+          />
       </LeftNav>
     );
   };
